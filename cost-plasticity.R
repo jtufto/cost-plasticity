1b816a55a4576ce482d978900f935cd61e83e48a
Jarle Tufto <jarle.tufto@ntnu.no>
2020-03-17 14:18:28 +0100

    Random midparental and individual slopes and elevations

--- cost-plasticity.R
+++ cost-plasticity.R
@@ -1,33 +1,40 @@
 library(MASS)
 library(TMB)
-library(glmmTMB)
 compile("cost_plasticity.cpp")
 dyn.load(dynlib("cost_plasticity"))
 
 # Experimental design
-m <- 100 # number of families
+m <- 1000 # number of families
 i <- rep(1:m, each=10) 
 n <- length(i)
 
 # True parameter values
-G <- diag(c(1,.1)) # G-matrix
-sigma_E <- .1
+G <- diag(c(1,.2)) # G-matrix
+sigma_E <- 1
 abbarbar <- c(0,.5) # mean elevation and slope in the population
 
-# Simulated data
+# Simulated data (will add fitness data later)
 set.seed(1)
 ab <- mvrnorm(m, abbarbar, G/2)[i,] + mvrnorm(n, c(0,0), Sigma = G/2) # individual elevations and slopes
 epsilon <- rnorm(n) # environment of development
 z <- ab[,1] + ab[,2]*epsilon + rnorm(n, sd=sigma_E) # phenotypes
 
-
+# Compute marginal likelihood
 data <- list(z=z, 
              epsilon=epsilon, 
-             i=i)
-parameters <- list(a=abbarbar[1],
-                   b=abbarbar[2],
-                   logSigma=0)
-obj <- MakeADFun(data, parameters, DLL="cost_plasticity")
+             i=i-1) 
+parameters <- list(abbarbar=abbarbar,
+                   log_sigma_E=0,
+                   log_sigma_ab=c(0,0),
+                   rho=0,
+                   abbar=matrix(0,m,2),
+                   ab=matrix(0,n,2))
+obj <- MakeADFun(data, parameters, DLL="cost_plasticity", random = c("abbar","ab"))
+
+# Fit the model
 fit <- nlminb(obj$par, obj$fn, obj$gr)
 fit$par
-lm(z~epsilon)
+rep <- sdreport(obj)
+summary(rep,"report")
+
+
--- cost_plasticity.cpp
+++ cost_plasticity.cpp
@@ -2,13 +2,46 @@
 template<class Type>
 Type objective_function<Type>::operator() ()
 {
+  using namespace density;
+  
+  // Data //
   DATA_VECTOR(z);
   DATA_VECTOR(epsilon);
-  PARAMETER(a);
-  PARAMETER(b)
-  PARAMETER(logSigma);
-  ADREPORT(exp(2*logSigma));
-  Type nll = -sum(dnorm(z, a+b*epsilon, exp(logSigma), true));
+  DATA_FACTOR(i);
+  
+  // Parameters //
+  PARAMETER_VECTOR(abbarbar);
+  PARAMETER(log_sigma_E); Type sigma_E = exp(log_sigma_E); ADREPORT(sigma_E);
+  PARAMETER_VECTOR(log_sigma_ab); 
+  vector<Type> sigma_half = exp(log_sigma_ab)/sqrt(2); 
+  vector<Type> sigma_ab = exp(log_sigma_ab); ADREPORT(sigma_ab)
+  PARAMETER_VECTOR(rho);
+  matrix<Type> Rho(2,2);
+  Rho = UNSTRUCTURED_CORR(rho).cov();
+  ADREPORT(Rho); 
+  
+  // Random effects //
+  PARAMETER_MATRIX(abbar);
+  PARAMETER_MATRIX(ab);
+  
+  Type nll = 0;
+  // Likelihood contribution from density of familywise midparental values) //
+  for (int i=0; i < abbar.rows(); i++) {
+    vector<Type> abbari = abbar.row(i);
+    vector<Type> error = abbari - abbarbar;
+    nll += VECSCALE(UNSTRUCTURED_CORR(rho),sigma_half)(error);
+  }
+  
+  for (int j=0; j < z.size(); j++) {
+    // Likelihood contributions from density of individual breeding values conditional on midparental values
+    vector<Type> abj = ab.row(j);
+    vector<Type> abbari = abbar.row(i(j));
+    vector<Type> error = abj - abbari;
+    nll += VECSCALE(UNSTRUCTURED_CORR(rho),sigma_half)(error);
+    
+    // Likelihood contribution from density of individual phenotypes conditional on breeding values
+    nll -= dnorm(z(j), ab(j,0) + ab(j,1)*epsilon(j), sigma_E, true);
+  }
+
   return nll;
 }
-
